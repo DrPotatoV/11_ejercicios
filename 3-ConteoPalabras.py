947be01c8958ae9b2c932a23d7d97f5764564fdd
DrPotatoV <a1231124@uabc.edu.mx>
2021-03-23 10:40:18 -0700

    Ya jala

---
+++ 3-ConteoPalabras.py
@@ -0,0 +1,9 @@
+#Conteo de palabras en python
+palabra = input("En que está pensando? ")
+total = print(len(palabra))
+
+palabrascont = palabra.split()
+count = len(palabrascont)
+print(palabra.split())
+
+print("El número de palabras de tu texto es: ", count)
